7e1c5ca8df0a167265604279ec6d93e9f879a93b
Junbo Wang <juniwang@microsoft.com>
2016-01-15 17:46:20 +0800

    Update solution.js
    first and simple trial

--- solution.js
+++ solution.js
@@ -1,1 +1,20 @@
 
+{
+    init: function(elevators, floors) {
+        var elevator = elevators[0]; // Let's use the first elevator
+        
+        floors.forEach(function(floor){
+            floor.on("up_button_pressed down_button_pressed", function() {
+                elevator.goToFloor(floor.floorNum())
+            })
+        }) 
+      
+        elevator.on("floor_button_pressed", function(floorNum) {
+            // Maybe tell the elevator to go to that floor?
+            elevator.goToFloor(floorNum)
+        })
+    },
+    update: function(dt, elevators, floors) {
+        // We normally don't need to do anything here
+    }
+}
